e990d655577fff4fce02ed652c6da16e781fd19b
dandenseven <knight1seven@gmail.com>
2025-09-22 21:17:53 -0400

    added in few objects in the store.py and grocery list.py

--- backend/models/grocery_lists.py
+++ backend/models/grocery_lists.py
@@ -6,22 +6,28 @@ class grocery_list():
 
     grocery_list_ref = ""
 
-    def __init__(self, store_name="", store_items="", store_shopped_items="", store_missing_items="", store_shopping_list="",
-                  store_id=None, user_id=None):
+    def __init__(self, store_name="", store_item="", store_item_price="", store_shopped_item="", store_shopped_item_price="", 
+                 store_missing_item="", store_missing_item_price="",store_shopping_list="",store_id=None, user_id=None):
         
         self.store_name = store_name
-        self.store_items = store_items
-        self.store_shopped_items = store_shopped_items
-        self.store_missing_items = store_missing_items
+        self.store_item = store_item
+        self.store_item_price = store_item_price
+        self.store_shopped_item = store_shopped_item
+        self.store_shopped_item_price =  store_shopped_item_price
+        self.store_missing_item = store_missing_item
+        self.store_missing_item_price = store_missing_item_price
         self.store_shopping_list = store_shopping_list
         self.store_id = store_id
         self.user_id = user_id
 
     def to_json(self):
         return{"store_name": self.store_name,
-            "store_items": self.store_items,
-            "store_shopped_items": self.store_shopped_items,
-            "store_missing_items": self.store_missing_items,
+            "store_item": self.store_item,
+            "store_item_price": self.store_item_price,
+            "store_shopped_item": self.store_shopped_item,
+            "store_shopped_item_price":self.store_shopped_item_price,
+            "store_missing_item": self.store_missing_item,
+            "store_missing_item_price":self.store_missing_item_price,
             "store_shopping_list": self.store_shopping_list,
             "store_id": self.store_id,
             "user_id": self.user_id
--- backend/models/stores.py
+++ backend/models/stores.py
@@ -6,14 +6,16 @@ class Stores:
 
     stores_ref = ""
 
-    def __init__(self, store_name="", store_address="", store_hours="", store_items="", store_sale_items="", 
-                 sales_promotions_start_date="", sales_promotions_end_date="", store_id=None):
+    def __init__(self, store_name="", store_address="", store_hours="", store_item="", store_item_price="", store_sale_item="", 
+                 store_sale_item_price="",sales_promotions_start_date="", sales_promotions_end_date="", store_id=None):
 
         self.store_name = store_name      
         self.store_address = store_address
         self.store_hours = store_hours
-        self.store_items = store_items
-        self.store_sale_items = store_sale_items
+        self.store_item = store_item
+        self.store_item_price = store_item_price
+        self.store_sale_item = store_sale_item
+        self.store_sale_item_price = store_sale_item_price
         self.sales_promotions_start_date = sales_promotions_start_date
         self.sales_promotions_end_date = sales_promotions_end_date
         self.store_id = store_id
@@ -23,8 +25,10 @@ class Stores:
         return{"store_name": self.store_name,
                 "store_address=": self.store_address,
                 "store_hours=": self.store_hours,
-                "store_items": self.store_items,
-                "store_sale_items": self.store_sale_items,
+                "store_item": self.store_item,
+                "store_item_price": self.store_item_price,
+                "store_sale_item": self.store_sale_item,
+                "store_sale_item_price": self.store_sale_item_price,
                 "self.sales_promotions_start_date": self.sales_promotions_start_date,
                 "self.sales_promotions_end_date": self.sales_promotions_end_date,
                 "store_id": self.store_id
