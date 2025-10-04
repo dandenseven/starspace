eab11d92c2f9ef60df25ab30b38178d25138fd1e
dandenseven <knight1seven@gmail.com>
2025-10-04 17:29:26 -0400

    updated models grocery_list.py

--- backend/models/grocery_lists.py
+++ backend/models/grocery_lists.py
@@ -6,16 +6,13 @@ class grocery_list():
 
     grocery_list_ref = ""
 
-    def __init__(self, store_name="", store_item="", store_item_price=int, store_shopped_item="", store_shopped_item_price=int, 
-                 store_missing_item="", store_missing_item_price=int,store_shopping_list="",store_shoppping_list_total=int, store_shopping_list_history="",store_item_favorites="",store_id=None, user_id=None):
+    def __init__(self, store_name="", store_items=[], store_shopped_items=[], store_missing_items=[],store_shopping_list=[],
+                 store_shoppping_list_total=int, store_shopping_list_history=[],store_item_favorites=[],store_id=None, user_id=None):
         
         self.store_name = store_name
-        self.store_item = store_item
-        self.store_item_price = store_item_price
-        self.store_shopped_item = store_shopped_item
-        self.store_shopped_item_price =  store_shopped_item_price
-        self.store_missing_item = store_missing_item
-        self.store_missing_item_price = store_missing_item_price
+        self.store_items = store_items
+        self.store_shopped_items = store_shopped_items
+        self.store_missing_items = store_missing_items
         self.store_shopping_list = store_shopping_list
         self.store_shoppping_list_total = store_shoppping_list_total
         self.store_shopping_list_history = store_shopping_list_history
@@ -25,18 +22,15 @@ class grocery_list():
                                                                                
     def to_json(self):
         return{"store_name": self.store_name,
-            "store_item": self.store_item,
-            "store_item_price": self.store_item_price,
-            "store_shopped_item": self.store_shopped_item,
-            "store_shopped_item_price":self.store_shopped_item_price,
-            "store_missing_item": self.store_missing_item,
-            "store_missing_item_price":self.store_missing_item_price,
-            "store_shopping_list": self.store_shopping_list,
-            "store_shoppping_list_total": self.store_shoppping_list_total,
-            "store_shopping_list_history": self.store_shopping_list_history,
-            "store_item_favorites": self.store_item_favorites,
-            "store_id": self.store_id,
-            "user_id": self.user_id
+               "store_items": self.store_items,
+               "store_shopped_items": self.store_shopped_items,
+               "store_missing_items": self.store_missing_items,
+               "store_shopping_list": self.store_shopping_list,
+               "store_shoppping_list_total": self.store_shoppping_list_total,
+               "store_shopping_list_history": self.store_shopping_list_history,
+               "store_item_favorites": self.store_item_favorites,
+               "store_id": self.store_id,
+               "user_id": self.user_id
         }
     
 
